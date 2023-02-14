c18d77cb06fbb54f5b0bf0926aa99034333feeed
Thomas Viot <viotthomas7@gmail.com>
2023-02-14 17:28:08 +0100

    crop

---
+++ src/Utilitaire/EasyCrop/Crop.js
@@ -0,0 +1,131 @@
+import React, { useState, useCallback } from 'react'
+import Cropper from 'react-easy-crop'
+import Slider from '@material-ui/core/Slider'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import { withStyles } from '@material-ui/core/styles'
+import { getOrientation } from 'get-orientation/browser'
+import { getCroppedImg, getRotatedImage } from './CanvasUtils'
+import { styles } from './style'
+
+import './Crop.css'
+
+
+const ORIENTATION_TO_ANGLE = {
+  '3': 180,
+  '6': 90,
+  '8': -90,
+}
+
+const Crop = ( { classes, onChangePhoto, closePopup }) => {
+  const [imageSrc, setImageSrc] = useState(null)
+  const [crop, setCrop] = useState({ x: 0, y: 0 })
+  const [rotation, setRotation] = useState(0)
+  const [zoom, setZoom] = useState(1)
+  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null)
+  const [croppedImage, setCroppedImage] = useState(null)
+
+  const onCropComplete = useCallback((croppedArea, croppedAreaPixels) => {
+    setCroppedAreaPixels(croppedAreaPixels)
+  }, [])
+
+  const saveCroppedImage = useCallback(async () => {
+    try {
+      const croppedImage = await getCroppedImg(
+        imageSrc,
+        croppedAreaPixels,
+        rotation
+      )
+      console.log('done', { croppedImage })
+      setCroppedImage(croppedImage)
+      onChangePhoto(croppedImage)
+      closePopup()
+      console.log("onchagephoto: ", onChangePhoto);
+    } catch (e) {
+      console.error(e)
+    }
+  }, [imageSrc, croppedAreaPixels, rotation]);
+
+  const onFileChange = async (e) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0]
+      let imageDataUrl = await readFile(file)
+
+      try {
+        // apply rotation if needed
+        const orientation = await getOrientation(file)
+        const rotation = ORIENTATION_TO_ANGLE[orientation]
+        if (rotation) {
+          imageDataUrl = await getRotatedImage(imageDataUrl, rotation)
+        }
+      } catch (e) {
+        console.warn('failed to detect the orientation')
+      }
+
+      setImageSrc(imageDataUrl)
+    }
+  }
+
+  return (
+    <div>
+      {imageSrc ? (
+        <React.Fragment>
+          <div className={classes.cropContainer}>
+            <Cropper
+              image={imageSrc}
+              crop={crop}
+              rotation={rotation}
+              zoom={zoom}
+              aspect={1.06/1}
+              onCropChange={setCrop}
+              onRotationChange={setRotation}
+              onCropComplete={onCropComplete}
+              onZoomChange={setZoom}
+            />
+          </div>
+          <div className={classes.controls}>
+            <div className={classes.sliderContainer}>
+              <Typography
+                variant="overline"
+                classes={{ root: classes.sliderLabel }}
+              >
+                Zoom
+              </Typography>
+              <Slider
+                value={zoom}
+                min={1}
+                max={3}
+                step={0.1}
+                aria-labelledby="Zoom"
+                classes={{ root: classes.slider }}
+                onChange={(e, zoom) => setZoom(zoom)}
+              />
+            </div>
+            <Button
+              onClick={saveCroppedImage}
+              variant="contained"
+              color="primary"
+              classes={{ root: classes.cropButton }}
+            >
+              Valider sélection
+            </Button>
+          </div>
+        </React.Fragment>
+      ) : (
+        <input type="file" onChange={onFileChange} accept="image/*"  className='inputCrop'/>
+      )}
+    </div>
+  )
+}
+
+function readFile(file) {
+  return new Promise((resolve) => {
+    const reader = new FileReader()
+    reader.addEventListener('load', () => resolve(reader.result), false)
+    reader.readAsDataURL(file)
+  })
+}
+
+export const StyledDemo = withStyles(styles)(Crop)
+
+
--- src/components/PopupDimImg/PopupDimImg.js
+++ src/components/PopupDimImg/PopupDimImg.js
@@ -1,30 +1,13 @@
-import React, {useState} from 'react'
-
-import CropImage from '../../Utilitaire/Crop/CropImage'
-
+import React from 'react'
+import { StyledDemo } from '../../Utilitaire/EasyCrop/Crop'
 import './PopupDimImg.css'
-import saveImage from '../../Utilitaire/saveImage'
-
 
 function PopupDimImg(props) {
-
-  const [variable, setVariable] = useState(null);
-
-  function changeSize(newSize){
-    setVariable(newSize);
-  }
-
-  async function saveSelected(){
-    await saveImage(variable);
-    console.log(variable);
-    props.closePopup();
-  }
   
   return (
     <div className='popup-img-container'>
         <div className='popup-img-body'>
-            <CropImage onCrop={changeSize}/>
-            {variable && (<button onClick={saveSelected} className="valider">Valider sélection</button>)}
+            <StyledDemo closePopup={props.closePopup} onChangePhoto={props.onChangePhoto}/>
         </div>
     </div>
   )
